596042d341d4d0ba0d3ade7551d1b4d97772107d
bjarnig <bjarnig@gmail.com>
2022-04-13 20:36:02 +0200

    Test

--- src/components/courses/PMA.js
+++ src/components/courses/PMA.js
@@ -21,7 +21,7 @@ class Pma extends Component {
 <article>
   <div className="date"> 31.03.2022 </div>
   <div className="hr"><hr /></div>
-  <h1>Class 18, GUI</h1>
+  <h1>Class 18,  GUI</h1>
   <a href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2021-2022/pma18.zip" target="_blank">
   <OpacityImage imageUrl= "https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2021-2022/pma18.png" />
   </a>
