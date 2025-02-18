1f1cb3d20473f968b5241088204bdd132e938b93
bjarnig <bjarnig@gmail.com>
2025-02-18 11:57:26 +0100

    latest

--- src/components/courses/PMB.js
+++ src/components/courses/PMB.js
@@ -788,6 +788,16 @@ class Pmb extends Component {
 */}
 
 <article>
+  <div className="date"> 18.02.2025 </div>
+  <div className="hr"><hr /></div>
+  <h1>Class 13, Live Coding</h1>
+  <a href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2024-2025/pmb13.zip" target="_blank">
+  <OpacityImage imageUrl= "https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2024-2025/pmb13.png" />
+  </a>
+  <p><a href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2024-2025/pmb13.zip" target="_blank">Download materials</a></p>
+</article>
+
+<article>
   <div className="date"> 04.02.2025 </div>
   <div className="hr"><hr /></div>
   <h1>Class 12, CDP 4</h1>
