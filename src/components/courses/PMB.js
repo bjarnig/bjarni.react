259b6ba5cd18abdc7d7e440987295b8e6a6dbd1b
bjarnig <bjarnig@gmail.com>
2025-09-30 10:54:33 +0200

    latest

--- src/components/courses/PMB.js
+++ src/components/courses/PMB.js
@@ -9,11 +9,34 @@ class Pmb extends Component {
       <div className="container">
         <div className="content">
         <article>
+            <div className="date"> 30.09.2025 </div>
+            <div className="hr">
+              <hr />
+            </div>
+            <h1>Class 03, Granular Examples</h1>
+            <a
+              href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2025-2026/pmb03.zip"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <OpacityImage imageUrl="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2025-2026/pmb03.png" />
+            </a>
+            <p>
+              <a
+                href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2025-2026/pmb03.zip"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download materials
+              </a>
+            </p>
+          </article>
+        <article>
             <div className="date"> 16.09.2025 </div>
             <div className="hr">
               <hr />
             </div>
-            <h1>Class 02, Programming and Music 2</h1>
+            <h1>Class 02, Granularity</h1>
             <a
               href="https://s3.eu-central-1.amazonaws.com/bjarnigwebdocs/2025-2026/pmb02.zip"
               target="_blank"
