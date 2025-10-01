6d65511207eb0f40be154918a1f1a3c0f2eeafd6
bjarnig <bjarnig@gmail.com>
2025-10-01 12:48:18 +0200

    Latest

---
+++ build/precache-manifest.36b110128ef1e22121a9c51f04a22cd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b82522bf9c598a889df071e2c137fee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f5a3fbf1731792bfc2a",
+    "url": "./static/css/main.39be9aba.chunk.css"
+  },
+  {
+    "revision": "c2efd477a30dbeaa2bb8",
+    "url": "./static/js/2.12899fe6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.12899fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5a3fbf1731792bfc2a",
+    "url": "./static/js/main.b739b908.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
